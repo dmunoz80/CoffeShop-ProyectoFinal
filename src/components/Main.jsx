fc6c4c951cc382ca1288224244f9e2cb74c42e78
Marcelo Perez <perezmarcelo363@gmail.com>
2023-07-19 15:14:18 -0400

    modificacion navbar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,16 @@ import Carousel from 'react-bootstrap/Carousel';
 import img1 from '../assets/images/imagen1.png';
 import img2 from '../assets/images/imagen2.png';
 import img3 from '../assets/images/imagen3.png';
+import { Container } from 'react-bootstrap';
 
 
 function Main() {
 
     return (
-
     <Carousel>
     <Carousel.Item>
         <img
-        className="d-block w-100"
+        className="w-100"
         src={img1}
         alt="First slide"
         />
@@ -20,7 +20,7 @@ function Main() {
 
     <Carousel.Item>
         <img
-        className="d-block w-100"
+        className=" w-100"
         src={img2}
         alt="Second slide"
         />
@@ -28,12 +28,13 @@ function Main() {
 
     <Carousel.Item>
         <img
-        className="d-block w-100"
+        className=" w-100"
         src={img3}
         alt="Third slide"
         />
     </Carousel.Item>
     </Carousel>
+    
 );
 }
 
