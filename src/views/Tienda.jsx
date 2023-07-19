c677be33a955edecca681e7c95014509e15612ab
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-07-19 14:59:37 -0400

    Se crea la vista tienda

---
+++ src/views/Tienda.jsx
@@ -0,0 +1,15 @@
+import ProductCard from "../components/ProductCard";
+import FooterNavigation from "../components/FooterNavigation";
+import NavbarTienda from "../components/NavbarTienda";
+
+const Tienda = () => {
+    return (
+        <>
+            <NavbarTienda />
+            <ProductCard />
+            <FooterNavigation />
+        </>
+    )
+}
+
+export default Tienda;
