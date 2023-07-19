ece2ea0e79336a71d32aad2602b49ec3eded0d2b
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-07-19 14:57:02 -0400

    cambio navlink de products a tienda

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
           <Nav className="me-auto">
             <NavLink to='/' className='text-decoration-none'>HOME</NavLink>
             <NavLink to='/' className='text-decoration-none'>NOSOTROS</NavLink>
-            <NavLink to='/Products' className='text-decoration-none'>PRODUCTOS</NavLink>
+            <NavLink to='/Tienda' className='text-decoration-none'>TIENDA</NavLink>
             <NavLink to='/' className='text-decoration-none'>COMENTARIOS</NavLink>
             <NavLink to='/' className='text-decoration-none'>CONTACTO</NavLink>
           </Nav>
