da714d120b55839104e55de03fdff512f13e4204
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-07-19 19:28:44 -0400

    cambio de logo

--- src/components/NavbarTienda.jsx
+++ src/components/NavbarTienda.jsx
@@ -2,7 +2,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../assets/images/Logo.png';
+import logo from '../assets/images/navbar-logo.png';
 
 function NavbarTienda() {
   return (
