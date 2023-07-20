0d882ed3ec34fdf9d6a05c9c133b5cd1b44062ac
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-07-19 20:04:58 -0400

    modificacion footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
+
+
 
 function Footer() {
   return (
@@ -13,11 +14,7 @@ function Footer() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to='/' className='text-decoration-none'>HOME</NavLink>
-            <NavLink to='/' className='text-decoration-none'>NOSOTROS</NavLink>
-            <NavLink to='/Tienda' className='text-decoration-none'>TIENDA</NavLink>
-            <NavLink to='/' className='text-decoration-none'>COMENTARIOS</NavLink>
-            <NavLink to='/' className='text-decoration-none'>CONTACTO</NavLink>
+
           </Nav>
         </Navbar.Collapse>
         <Navbar.Text className='text-light m-2'>Ⓒ 2023.Cofee Shop Company. Reservados todos los derechos</Navbar.Text>
