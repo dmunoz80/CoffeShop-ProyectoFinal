fb203166d2cbdeee72c2514693f6c9ed58b0b7b7
Marcelo Perez <perezmarcelo363@gmail.com>
2023-07-22 13:44:22 -0400

    se añade info e imagen en nosotros

--- src/components/NosotrosTexto.jsx
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-function texto() {
-
-  
-}
-
-export default texto;
--- src/views/Nosotros.jsx
+++ src/views/Nosotros.jsx
@@ -1,11 +1,37 @@
 import Navbar from "../components/Navbar";
+import {Container, Row, Col} from "react-bootstrap"
+import nosotrosCafeteria from "../assets/images/nosotrosCafeteria.jpg"
+import InfoNosotros from "../components/InfoNosotros";
 
 const Nosotros = () => {
-    return (
-        <>
-            <Navbar title={"Sobre Nosotros"} />
-        </>
-    )
+  return (
+    <>
+      <Navbar title={"Nosotros"} />
+      <Container className="mt-5">
+        <Row>
+          <Col>
+             <InfoNosotros
+              title={"Nuestra Historia"}
+              content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus neque euismod quam rhoncus, at blandit nulla ultricies. Pellentesque sollicitudin nunc purus, id ornare lorem condimentum eu. Mauris lacinia eu dui at lobortis. Phasellus ex arcu, finibus et justo eget, aliquet rhoncus enim. Nulla ipsum orci, pretium eu tellus nec, imperdiet sodales lorem."}
+             />
+            <InfoNosotros
+              title={"Nuestra Historia"}
+              content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus neque euismod quam rhoncus, at blandit nulla ultricies. Pellentesque sollicitudin nunc purus, id ornare lorem condimentum eu. Mauris lacinia eu dui at lobortis. Phasellus ex arcu, finibus et justo eget, aliquet rhoncus enim. Nulla ipsum orci, pretium eu tellus nec, imperdiet sodales lorem."}
+             />
+             <InfoNosotros
+              title={"Nuestra Historia"}
+              content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus neque euismod quam rhoncus, at blandit nulla ultricies. Pellentesque sollicitudin nunc purus, id ornare lorem condimentum eu. Mauris lacinia eu dui at lobortis. Phasellus ex arcu, finibus et justo eget, aliquet rhoncus enim. Nulla ipsum orci, pretium eu tellus nec, imperdiet sodales lorem. "}
+             />
+          </Col>
+          <Col>
+          <img src={nosotrosCafeteria} alt="" style={{borderRadius: "50px"}}></img>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
-
+  
 export default Nosotros;
+
+
