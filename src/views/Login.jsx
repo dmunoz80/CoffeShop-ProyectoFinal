e5b38be6df7158d74c060c0f455bd526401b48b3
Felipe P <fpalmasaldivia@gmail.com>
2023-07-23 15:30:19 -0400

    se da estilo a vista Login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import Formlogin from "../components/Formlogin"
 import FooterNavigation from '../components/FooterNavigation';
 import Navbar from "../components/Navbar";
+import { Container, Row, Col } from "react-bootstrap"
+import fotologin from "../assets/images/fotologin.jpeg"
+
 
 const Login = () => {
     return (
         <div>
             <Navbar />
-            <Formlogin />
+            <Container>
+                <Row>
+                    <Col>
+                        <Formlogin />
+                    </Col>
+                    <Col>
+                    <img src={fotologin} alt='' style={{ width:"250" , borderRadius: "50px", margin: "10px" }}></img>
+                    </Col>
+                </Row>
+            </Container>
             <FooterNavigation />
 
         </div>
