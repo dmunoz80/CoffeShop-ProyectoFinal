56e4cdfd25927a61957f5ef9e9175882dde33547
Felipe P <fpalmasaldivia@gmail.com>
2023-07-28 19:57:50 -0400

    actualizacion con bootstrap

--- src/components/Comentarios.jsx
+++ src/components/Comentarios.jsx
@@ -1,23 +1,23 @@
 
 import React from 'react';
 import comentariosData from '../comentarios.json';
+import { Card } from 'react-bootstrap';
 
 const ComentariosCard = () => {
     return (
         <div className="container">
             <div className="row">
                 {comentariosData.comentarios.map((comentario, index) => (
-                    <div style={{borderColor:'#b4764f'}} key={index} className="col-md-6">
-                        <div className="card my-3" >
-                            <div className="card-body"  >
-                                <h5 className="card-title">{comentario.usuario}</h5>
-                                <hr/>
-                                <h6 className="card-subtitle mb-2 text-muted">{comentario.producto}</h6>
-                                <hr/>
-                                <p className="card-text">{comentario.comentario}</p>
-                            </div>
-                        </div>
-                    </div>
+
+                    <Card className='mt-5' style={{ width: '600px', borderColor: "#b4764f", marginLeft: '20px', marginBottom: '20px' }}>
+                        <Card.Body>
+                            <Card.Title>{comentario.usuario}</Card.Title>
+                            <hr />
+                            <Card.Text>{comentario.producto}</Card.Text>
+
+                            <Card.Text>{comentario.comentario}</Card.Text>
+                        </Card.Body>
+                    </Card>
                 ))}
             </div>
         </div>
