c8fb0d9d54338d05c46a08e2466ba9fff820afc7
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-07-28 08:51:48 -0400

    actualizaciones

--- src/views/Comments.jsx
+++ src/views/Comments.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import FooterNavigation from '../components/FooterNavigation';
 import Navbar from "../components/Navbar";
-import { Container, Row, Col } from "react-bootstrap"
 import Comentarios from "../components/Comentarios"
 
 
 const Comments = () => {
     return (
-        <div >
+   <>
             <Navbar title={"Comentarios"} />
+            
             <div style={{ backgroundColor: "#cd966c" }} >
-
-                            < Comentarios />
-
-       
+            < Comentarios />
             </div>
+
             <FooterNavigation />
-        </div >
+            </>
     );
 }
 
