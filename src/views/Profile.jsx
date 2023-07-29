7e770fa08681723a040c5ff0055eb4622c791343
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-07-29 12:18:47 -0400

    actualizacion profile

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import Navbar from "../components/Navbar";
+import FooterNavigation from "../components/FooterNavigation";
+import {Row, Container, Col, Button, Card} from "react-bootstrap"
+import CardProfile from '../components/CardProfile';
 
 
 // Credenciales de usuarios (ejemplo)
 const usersData = [
-    { id: 1, username: 'john_doe', password: 'password123', name: 'John Doe' },
-    { id: 2, username: 'jane_smith', password: 'password456', name: 'Jane Smith' }
+    { id: 1, username: 'john_doe', password: 'password123', name: 'John', secondname: 'Doe', email: 'johndoe@correo.com', address:'Av.Providencia, Santiago, Chile' },
+    { id: 2, username: 'jane_smith', password: 'password456', name: 'Jane', secondname: 'Smith', email: 'janesmith@correo.com', address:'Av.Pedro Montt, Valparaíso, Chile'  }
 ];
 
 function UserItem({ user }) {
@@ -12,6 +16,10 @@ function UserItem({ user }) {
         <div className="user-item">
             <h2>{user.name}</h2>
             <p><strong>Nombre de usuario:</strong> {user.username}</p>
+            <p><strong>Nombre:</strong> {user.name}</p>
+            <p><strong>Apellido:</strong> {user.secondname}</p>
+            <p><strong>Correo:</strong> {user.email}</p>
+            <p><strong>Dirección:</strong> {user.address}</p>
         </div>
     );
 }
@@ -83,16 +91,22 @@ function App() {
     };
 
     return (
-        <div className="App">
-            {loggedInUser ? (
-                <div>
-                    <h1>Bienvenido</h1>
-                    <UserItem user={loggedInUser} />
-                    <button onClick={handleLogout}>Cerrar sesión</button>
-                </div>
-            ) : (
-                <LoginForm onLogin={handleLogin} />
-            )}
+       <div className="container-fluid justify-content-center p-0">
+            <Navbar title={"MI PERFIL"} />
+            <div>
+            <CardProfile />
+                        {loggedInUser ? (
+                        <div className='d-flex flex-column m-5'>
+                        <h1>Bienvenido</h1>
+                        <UserItem user={loggedInUser} />
+                        <Button onClick={handleLogout} style={{backgroundColor:"#874421", borderColor:"#874421"}} >Cerrar sesión</Button>
+                        </div>
+                        ) : (
+                        <LoginForm onLogin={handleLogin} />
+                        )}
+
+            </div>
+            <FooterNavigation />
         </div>
     );
 }
