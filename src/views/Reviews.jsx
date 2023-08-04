f95a9b669ded5c23d8c73225ec553d970a30ae45
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-08-04 17:50:35 -0400

    actualizacion

--- src/views/Reviews.jsx
+++ src/views/Reviews.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import FooterNavigation from '../components/FooterNavigation';
 import Navbar from "../components/Navbar";
 import Comentarios from "../components/Comentarios"
-//import fondocomentario from "../assets/images/fondocomentario.jpg"
-
 
 const Reviews = () => {
+
     return (
         <>
             <Navbar title={"RESEÑAS DE CLIENTES"} />
