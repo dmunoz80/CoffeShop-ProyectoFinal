c41caaf2958b104c31a4b88e4ebc3aa9d92b4ec6
Felipe P <fpalmasaldivia@gmail.com>
2023-08-14 19:21:42 -0400

    actualizacion

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,89 +1,57 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect } from 'react';
-//import Button from 'react-bootstrap/Button';
-//import Card from 'react-bootstrap/Card';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 //import { useContext } from 'react';
 //import Context from '../Context';
 //import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
+const urlBaseServer = "http://localhost:3000";
 
-const MarketPlace = () => {
-    const [products, setProducts] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [isError, setIsError] = useState(false);
+const ProductCard = () => {
+    const [products , setProducts] = useState([]);
 
     const getProducts = async () => {
-        try {
-            const response = await axios.get('https://proyectofinaltiendaback.up.railway.app/products');
-            setProducts(response.data.data.products);
-            setIsError(false);
-        } catch (error) {
-            setIsError(true);
-        }
+        const { data: products } = await axios.get(urlBaseServer + "/comentarios");
+        setProducts([...products]);
     };
 
     useEffect(() => {
         getProducts();
     }, []);
 
-    const handleSearch = (event) => {
-        setSearchTerm(event.target.value);
-    };
-
-    const filteredProducts = products.filter((product) => {
-        return product.name.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-
     return (
-        <div>
-            {isError ?
-                (<div className="noconexion">
-                    -sin conexión al servidor-
-                </div>)
-                :
-                (<div>
-                    <div className='Search'>
-                        <i class="fa-solid fa-magnifying-glass fa-flip-horizontal icon"></i>
-                        <input
-                            type="text"
-                            className="form"
-                            placeholder="Busca tu juego aquí"
-                            value={searchTerm}
-                            onChange={handleSearch}
-                        />
-                    </div>
+        <div className="d-flex flex-wrap justify-content-center gap-3 p-3">
+            {products.map((products, index) => (
+                < Card className='mt-5 with-zoom' key={products.id} style={{ width: '16rem', borderColor: "#b4764f" }}>
+                    <Card.Img className='img-card' src={products.img} alt={products.name} />
+                    <Card.Body>
+                        <Card.Title className='fs-4 text-center text-capitalize' >{(products.name)}</Card.Title>
+                        <hr />
+                        <h5 className='text-center'>
+                            Precio: ${products.price}
+                        </h5>
+                        <div className='d-flex flex-column gap-3'>
+                            <Button
+                                variant="outline-dark"
+                                onClick={() => navigate(`/product/${products.id}`)}
+                                style={{ borderColor: "#b4764f" }}>
+                                Ver más
+                            </Button>
 
-                    <div className="container">
-                        {filteredProducts.map((product) => {
-                            const src = `/product/${product.id}`;
-                            return (
-                                <div className='product'>
-                                    <div key={product.id}>
-                                        <div>
-                                            <Link to={src}>
-                                                <img src={product.img} alt="" />
-                                            </Link>
-                                        </div>
-                                        <div className="productName">
-                                            <Link to={src}>
-                                                {product.name}
-                                            </Link>
-                                        </div>
-                                        <div className="productPrice">
-                                            ${product.price}
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>)
-            }
+                            <Button
+                                onClick={() => handleClick(products)}
+                                variant="outline-dark"
+                                style={{ borderColor: "#b4764f" }}>
+                                Añadir al carro
+                            </Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            ))}
         </div>
-    );
-    
+    )
+}
 
-};
-export default MarketPlace;
+export default ProductCard;
